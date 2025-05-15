7027b607cd1d209efe8b98e5735d697934285efd
ahmedadhem <ahmedadhem330@gmail.com>
2025-05-15 21:07:21 +0300

    commit

--- app/Components/ui/SectionTitle.tsx
+++ app/Components/ui/SectionTitle.tsx
@@ -16,7 +16,7 @@ export default function SectionTitle({
       <p className={`bg-emerald-200/50 rounded-lg text-sm w-fit mx-auto ${highlighted ? "px-3 py-1" : "px-0 py-0"}`}>
         {headline}
       </p>
-      <p className="text-center text-3xl sm:text-5xl font-bold text-accent my-4">
+      <p className="text-center text-3xl sm:text-5xl font-bold my-4">
         {title}
       </p>
       <p className="text-center text-gray-600 dark:text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
